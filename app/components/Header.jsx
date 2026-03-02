6ed04c43e0a158b92154eb7c54f37a614bd4438a
Shamiul40 <shamiul4040@gmail.com>
2026-03-02 23:14:43 +0600

    header create

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,9 +1,18 @@
+import Image from "next/image";
 
+import logo from "/public/assets/logo.svg"
 
 export default function Header() {
   return (
-    <div>
-      
-    </div>
+   <nav>
+        <div>
+            <Image src={logo} width={100} height={100} alt="logo" />
+        </div>
+        <ul>
+            <li>about</li>
+            <li>Contact Us</li>
+        </ul>
+        
+   </nav>
   )
 }
