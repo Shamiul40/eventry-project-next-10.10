9f0cbc0e61b7c37924cf3d3c658618c0e8fde474
Shamiul40 <shamiul4040@gmail.com>
2026-03-02 23:29:12 +0600

    header create

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,16 +1,17 @@
 import Image from "next/image";
 
 import logo from "/public/assets/logo.svg"
+import Link from "next/link";
 
 export default function Header() {
   return (
-   <nav>
-        <div>
-            <Image src={logo} width={100} height={100} alt="logo" />
-        </div>
-        <ul>
-            <li>about</li>
-            <li>Contact Us</li>
+   <nav className="flex justify-between items-center py-4  ">
+        <Link href="/">
+            <Image src={logo} width={150} height={150} alt="logo" />
+        </Link>
+        <ul className="flex justify-center items-center gap-2 text-[#9C9C9C] md:gap-3 lg:gap-4">
+           <Link href="/about"> <li>about</li></Link>
+            <Link href="/contact-us"><li>Contact Us</li></Link>
         </ul>
         
    </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ],
   theme: {
     container: {
       center: true,
