fc9e7cd0cbc14fdae0d4a800e2e56c2e0e6e9fc4
Shamiul40 <shamiul4040@gmail.com>
2026-03-02 23:52:30 +0600

    responsive

--- app/components/Landing/landingHeader.jsx
+++ app/components/Landing/landingHeader.jsx
@@ -3,7 +3,7 @@
 export default function LandingHeader() {
   return (
     <nav className="flex justify-between items-center">
-      <h1 className="text-2xl ">Discover Events</h1>
+      <h1 className="text-3xl ">Discover Events</h1>
         <div>
             <input className="bg-[#27292F] border border-[#CCCCCC]/20 py-1 px-2 rounded-md" type="text" placeholder="search"  />
         </div>
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
 
+
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
