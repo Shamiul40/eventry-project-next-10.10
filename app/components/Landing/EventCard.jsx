90ac79d616546feee9a9e1a9eada4d691c661e17
Shamiul40 <shamiul4040@gmail.com>
2026-03-03 23:38:34 +0600

    event card style done

--- app/components/Landing/EventCard.jsx
+++ app/components/Landing/EventCard.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import googleImage from "/public/assets/events/google-io-2023-1.png"
+import Link from 'next/link'
 
 export default function EventCard() {
   return (
@@ -8,7 +9,7 @@ export default function EventCard() {
           <Image src={googleImage} alt="Event 1" className="w-full" />
 
           <div className="p-3">
-            <a href="./details.html" className="font-bold text-lg">Google I/O Extended</a>
+            <Link href="./details.html" className="font-bold text-lg">Google I/O Extended</Link>
             <p className="text-[#9C9C9C] text-sm mt-1">Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh</p>
             <div className="text-[#737373] text-sm mt-1">
               <span>1k Interested</span>
