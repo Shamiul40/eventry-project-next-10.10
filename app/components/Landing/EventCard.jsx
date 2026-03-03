f9e7d0a1d0be5848176a053c61ebbce6c5b4d83c
Shamiul40 <shamiul4040@gmail.com>
2026-03-03 23:34:27 +0600

    image optimized

--- app/components/Landing/EventCard.jsx
+++ app/components/Landing/EventCard.jsx
@@ -1,9 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
+import googleImage from "/public/assets/events/google-io-2023-1.png"
 
 export default function EventCard() {
   return (
     <div className="overflow-hidden rounded-md bg-[#242526]">
-          <img src="./assets/events/google-io-2023-1.png" alt="Event 1" className="w-full" />
+          <Image src={googleImage} alt="Event 1" className="w-full" />
 
           <div className="p-3">
             <a href="./details.html" className="font-bold text-lg">Google I/O Extended</a>
