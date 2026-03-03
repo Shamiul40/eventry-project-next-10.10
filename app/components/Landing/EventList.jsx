9c0e1a4d9cc9c0da28da2ef2fd0730a17888de2d
Shamiul40 <shamiul4040@gmail.com>
2026-03-03 23:07:51 +0600

    event card design

--- app/components/Landing/EventList.jsx
+++ app/components/Landing/EventList.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import EventCard from './EventCard'
 
 export default function EventList() {
   return (
-    <div className=''>
-     
+    <div className='grid grid-cols-1'>
+        <EventCard></EventCard>
     </div>
   )
 }
