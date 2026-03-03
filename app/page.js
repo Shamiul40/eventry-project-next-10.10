dd078d7f2488a6e67fc2a772080656c3ae59ecbf
Shamiul40 <shamiul4040@gmail.com>
2026-03-03 23:03:37 +0600

    eventlist grid

--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import LandingHeader from "./components/Landing/LandingHeader";
+import EventList from "./components/Landing/EventList";
 
 export default function Home() {
   return (
-   <section>
-     <LandingHeader></LandingHeader>
-   </section>
+    <section>
+      <LandingHeader></LandingHeader>
+      <section>
+        <EventList></EventList>
+      </section>
+    </section>
   );
 }
