94f96955163a0e5a1a96f848223862bfeae5f01b
Shamiul40 <shamiul4040@gmail.com>
2026-03-04 21:26:48 +0600

    heroDetails components create

--- app/details/[id]/page.js
+++ app/details/[id]/page.js
@@ -1,8 +1,15 @@
+import DetailsComponents from "@/app/components/DetailsPage/Details";
+import DetailsVenue from "@/app/components/DetailsPage/DetailsVenue";
+import HeroDetails from "@/app/components/DetailsPage/HeroDetails";
 
 export default function DetailsPage() {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <HeroDetails></HeroDetails>
+        <section className="grid grid-cols-5 ">
+            <DetailsComponents></DetailsComponents>
+            <DetailsVenue></DetailsVenue>
+        </section>
+    </main>
   )
 }
