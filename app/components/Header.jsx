37faa3d258229aecfc936ccebbba633ae8fdcd25
Shamiul40 <shamiul4040@gmail.com>
2026-03-10 23:09:13 +0600

    searchbar

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -7,10 +7,10 @@ export default function Header() {
   return (
     <nav className="flex justify-between items-center py-4  ">
       <Link href="/">
-        <Image src={logo} width={120} height={120} alt="logo image " />
+        <Image src={logo} width={120} height={120} alt="logo image  " />
       </Link>
       <ul className="flex justify-center items-center gap-2 text-[#9C9C9C] md:gap-3 lg:gap-4">
-        <Link href="/about-us">
+        <Link href="/about">
      
           <li>about</li>
         </Link>
